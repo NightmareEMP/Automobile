4c659e37d7de373866f6307c5e33abbca2645a5e
NightmareEMP <wowjw2011@gmail.com>
2025-11-25 23:16:31 +0800

    11252025 test upload

---
+++ Phase1/Vehicle_Ctrl.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+#include <cstint>
+#include <cmath>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include "Vehicle_Ctrl.h"
---
+++ Phase1/inc/PCA9585.h
@@ -0,0 +1,11 @@
+#ifndef PCA9685_H
+#define PCA9685_H
+
+#include <cstdint>
+
+Class PCA9685{
+public:
+	
+
+
+
