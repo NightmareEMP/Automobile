616073d45f35ed017c5cbf077ffd7dc71259786b
NightmareEMP <wowjw2011@gamil.com>
2026-08-22 22:12:06 +0800

    Ignore ROS2 build artifacts

--- Phase4/ros2_ws/src/vehicle_interface/build/vehicle_interface/CMakeFiles/vehicle_interface.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vehicle_interface"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vehicle_interface.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
